7989f105617cf2d7906846733c92e6b2092c174d
Thomas Kim <tkimm156@gmail.com>
2024-02-07 16:38:17 -0600

    Write findSpotForCol()

--- connect4.js
+++ connect4.js
@@ -75,7 +75,10 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 5
-  return 5;
+  for(let y = HEIGHT - 1; y >= 0; y--) {
+    if(board[y][x] === null) return y;
+  }
+  return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
